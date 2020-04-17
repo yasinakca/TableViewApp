4186769c25689c48d80a3a6de617fcc836a080ac
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-04-17 12:40:04 +0300

    a swift app which shows how to use table view

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TableViewApp
+
+A basic swift app which is about how to use table view - a common object in the library- and how transfer data between table view and the other view controllers with segue.
--- TableViewApp/ViewController.swift
+++ TableViewApp/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var imageArray = [UIImage]()
+    var nameArray = [String]()
+    
+    var chosenName = ""
+    var chosenImage: UIImage?
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        nameArray.append("Colesseum")
+        nameArray.append("Greatwall")
+        nameArray.append("Kremlin")
+        nameArray.append("Stonehenge")
+        nameArray.append("Tajmahal")
+        
+        imageArray.append(UIImage(named: "colosseum.jpg")!)
+        imageArray.append(UIImage(named: "greatwall.jpg")!)
+        imageArray.append(UIImage(named: "kremlin.jpg")!)
+        imageArray.append(UIImage(named: "stonehenge.jpg")!)
+        imageArray.append(UIImage(named: "tajmahal.jpg")!)
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC" {
+            let destination = segue.destination as! SecondViewController
+            destination.selectedName = chosenName
+            destination.selectedImage = chosenImage
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenName = nameArray[indexPath.row]
+        chosenImage = imageArray[indexPath.row]
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
 
 }
 
